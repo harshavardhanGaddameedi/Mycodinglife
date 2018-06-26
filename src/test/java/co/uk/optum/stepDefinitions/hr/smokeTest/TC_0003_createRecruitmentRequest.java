9870084775530c92d836d431af4078916d58a8a8
MS\srevanu1 <sucharitha.revanuru@optum.com>
2018-06-26 12:32:23 +0100

    Step Definition Files

---
+++ src/test/java/co/uk/optum/stepDefinitions/hr/smokeTest/TC_0003_createRecruitmentRequest.java
@@ -0,0 +1,116 @@
+package co.uk.optum.stepDefinitions.hr.smokeTest;
+
+import cucumber.api.DataTable;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class TC_0003_createRecruitmentRequest {
+	@Given("^user launches the Idempiere application$")
+	public void user_launches_the_Idempiere_application() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^user logs in using the <Username> and <Password>$")
+	public void user_logs_in_using_the_Username_and_Password(DataTable arg1) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    // For automatic transformation, change DataTable to one of
+	    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+	    // E,K,V must be a scalar (String, Integer, Date, enum etc)
+	    throw new PendingException();
+	}
+
+	@When("^selects the role as LeadGP$")
+	public void selects_the_role_as_LeadGP() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^hits ok$")
+	public void hits_ok() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^the dashboard of LeadGP is displayed$")
+	public void the_dashboard_of_LeadGP_is_displayed() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^user Clicks on Menu button$")
+	public void user_Clicks_on_Menu_button() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^Selects 'Human resource & Payroll'$")
+	public void selects_Human_resource_Payroll() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^selects 'Human resource'$")
+	public void selects_Human_resource() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^selects 'Team View'$")
+	public void selects_Team_View() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^select the 'Clinical Pharmacist' position which was created$")
+	public void select_the_Clinical_Pharmacist_position_which_was_created() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^clicks on the Export Button \\(right arrow\\) on the right hand side of the Tool Bar of iDempiere$")
+	public void clicks_on_the_Export_Button_right_arrow_on_the_right_hand_side_of_the_Tool_Bar_of_iDempiere() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^Recruitment Request window opens up$")
+	public void recruitment_Request_window_opens_up() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^fills the 'Type of Recruitment', 'Business case' and 'Reason' fields$")
+	public void fills_the_Type_of_Recruitment_Business_case_and_Reason_fields() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^fills the 'Proposed Start Date' field$")
+	public void fills_the_Proposed_Start_Date_field() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^the status of the request is changed from \"([^\"]*)\" to \"([^\"]*)\"$")
+	public void the_status_of_the_request_is_changed_from_to(String arg1, String arg2) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^hits save$")
+	public void hits_save() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^the Recruitment Request for the 'Clinical Pharmacist' position is created awaiting approval$")
+	public void the_Recruitment_Request_for_the_Clinical_Pharmacist_position_is_created_awaiting_approval() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+
+}
---
+++ src/test/java/co/uk/optum/stepDefinitions/hr/smokeTest/TC_012b_OnboardingNSCL.java
@@ -0,0 +1,176 @@
+package co.uk.optum.stepDefinitions.hr.smokeTest;
+
+import cucumber.api.DataTable;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class TC_012b_OnboardingNSCL {
+
+	@Given("^I am in login Page$")
+	public void i_am_in_login_Page() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^I login to IDempiere with \"([^\"]*)\" \"([^\"]*)\"$")
+	public void i_login_to_IDempiere_with(String arg1, String arg2, DataTable arg3) throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    // For automatic transformation, change DataTable to one of
+	    // List<YourType>, List<List<E>>, List<Map<K,V>> or Map<K,V>.
+	    // E,K,V must be a scalar (String, Integer, Date, enum etc)
+	    throw new PendingException();
+	}
+
+	@Given("^selects the role as 'HRAdministrator'$")
+	public void selects_the_role_as_HRAdministrator() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Given("^hits ok$")
+	public void hits_ok() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^the dashboard of HRAdmin is displayed$")
+	public void the_dashboard_of_HRAdmin_is_displayed() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^user Clicks on Menu button$")
+	public void user_Clicks_on_Menu_button() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^Selects 'Human resource & Payroll'$")
+	public void selects_Human_resource_Payroll() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^selects 'Human resource'$")
+	public void selects_Human_resource() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^selects 'Requests'$")
+	public void selects_Requests() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^selects 'Onboarding Request'$")
+	public void selects_Onboarding_Request() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^the 'Onboarding requests'grid view page opens$")
+	public void the_Onboarding_requests_grid_view_page_opens() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^user selects and edits the 'Onboarding request'$")
+	public void user_selects_and_edits_the_Onboarding_request() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^the 'Onboarding Request' opens$")
+	public void the_Onboarding_Request_opens() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^user clicks on the 'Detail Record' at the bottom of the window$")
+	public void user_clicks_on_the_Detail_Record_at_the_bottom_of_the_window() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^the Applicant Tab opens with Applicant name, Open Payroll Contract Letter Button$")
+	public void the_Applicant_Tab_opens_with_Applicant_name_Open_Payroll_Contract_Letter_Button() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^Open New Starter Checklist Window Button$")
+	public void open_New_Starter_Checklist_Window_Button() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^User clicks on 'edit Record'$")
+	public void user_clicks_on_edit_Record() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^the 'Request > Applicant' window opens$")
+	public void the_Request_Applicant_window_opens() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^User clicks on 'Open New Starter Checklist Window' button$")
+	public void user_clicks_on_Open_New_Starter_Checklist_Window_button() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^Click on Ok button$")
+	public void click_on_Ok_button() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^'New Starter Checklist Window' opens$")
+	public void new_Starter_Checklist_Window_opens() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@When("^User clicks on edit button$")
+	public void user_clicks_on_edit_button() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^the 'Checklist' window opens$")
+	public void the_Checklist_window_opens() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^User fills in all details including mandatory Bank Details$")
+	public void user_fills_in_all_details_including_mandatory_Bank_Details() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^User clicks on save button$")
+	public void user_clicks_on_save_button() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^User clicks on 'Cleared to start' button$")
+	public void user_clicks_on_Cleared_to_start_button() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Then("^the applicant is onboarded in to the organization$")
+	public void the_applicant_is_onboarded_in_to_the_organization() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+}
