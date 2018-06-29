4ee094fcd77eddddf044a4f0782197a2bda68b14
GPEmpower <anand_pendyala@optum.com>
2018-06-29 15:53:47 +0530

    Updated

---
+++ src/test/java/co/uk/optum/stepDefinitions/hr/smokeTest/TC_0001_createNPR.java
@@ -0,0 +1,74 @@
+package co.uk.optum.stepDefinitions.hr.smokeTest;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
+
+public class TC_0001_createNPR {
+    @And("^I click on New Position Request$")
+    public void iClickOnNewPositionRequest() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @And("^I enter all the New Position Request Details$")
+    public void iEnterAllTheNewPositionRequestDetails() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @And("^I click on Save Button$")
+    public void iClickOnSaveButton() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @And("^I enter the following Details$")
+    public void iEnterTheFollowingDetails() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @And("^I click on Attachment icon on the Tool bar$")
+    public void iClickOnAttachmentIconOnTheToolBar() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @And("^I attach the Job Description of Clinical Pharmacist$")
+    public void iAttachTheJobDescriptionOfClinicalPharmacist() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @And("^I click on Tick mark of the Attachment Window$")
+    public void iClickOnTickMarkOfTheAttachmentWindow() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @Then("^the Job Description is attached$")
+    public void theJobDescriptionIsAttached() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @And("^I change the status to Pending Approval$")
+    public void iChangeTheStatusToPendingApproval() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+    @And("^I click on the Save Button$")
+    public void iClickOnTheSaveButton() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+
+    @Then("^the New Position Request is saved$")
+    public void theNewPositionRequestIsSaved() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException ();
+    }
+}
